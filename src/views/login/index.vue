fb698e6c45f797f534da8c94a63050a13a98462d
sslazy <1185790368@qq.com>
2023-04-24 18:34:30 +0800

    feat(custom): 引入axios与后端接口交互

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import { reactive } from 'vue'
+import { login } from '@/api/auth'
+const form = reactive({
+  username: 'admin',
+  password: '123456'
+})
+
+const handleLogin = () => {
+  login(form).then((res) => {
+    console.log(res.data.data)
+  })
+}
+</script>
 
 <template>
-  <router-view />
+  <div>
+    <el-button type="primary" size="default" @click="handleLogin">登录</el-button>
+  </div>
 </template>
-
-<style scoped></style>
