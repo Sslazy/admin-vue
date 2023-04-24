9bd30261066453d39a93092b757fa5b4f92da5e9
sslazy <1185790368@qq.com>
2023-04-24 18:18:08 +0800

    feat(custom): 配置自动导入

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const msg = ref('测试自动导入')
+</script>
 
 <template>
-  <router-view />
+  <div class="bg-sky-600 w-50 h-50 text-white f-center">
+    <p class="text-xl">{{ msg }}</p>
+  </div>
+  <hello></hello>
+  <el-empty description="description" />
 </template>
-
-<style scoped></style>
