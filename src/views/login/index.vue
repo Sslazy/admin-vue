f3cb8386d27ebe7542fea43e2a1e9e653bcd9364
sslazy <1185790368@qq.com>
2023-04-24 21:59:19 +0800

    feat(custom): 使用Pinia管理用户信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,20 +1,18 @@
 <script setup>
-import { reactive } from 'vue'
-import { login } from '@/api/auth'
 const form = reactive({
   username: 'admin',
   password: '123456'
 })
 
-const handleLogin = () => {
-  login(form).then((res) => {
-    console.log(res.data.data)
-  })
-}
+const store = useAdminStore()
+
+const { storeLogin, getStoreInfo, storeLogout } = store
 </script>
 
 <template>
   <div>
-    <el-button type="primary" size="default" @click="handleLogin">登录</el-button>
+    <el-button type="primary" size="default" @click="storeLogin(form)">登录</el-button>
+    <el-button type="primary" size="default" @click="getStoreInfo">获取用户信息</el-button>
+    <el-button type="primary" size="default" @click="storeLogout">登出</el-button>
   </div>
 </template>
