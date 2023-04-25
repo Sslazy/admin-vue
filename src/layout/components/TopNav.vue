20b323101772c8dd9de95e8dea01b7ff767eab4d
sslazy <1185790368@qq.com>
2023-04-25 13:14:35 +0800

    feat(custom): 侧边栏伸缩+顶部功能按钮（全屏\刷新）

--- src/layout/components/TopNav.vue
+++ src/layout/components/TopNav.vue
@@ -1,26 +1,37 @@
 <script setup>
 const store = useAdminStore()
-const { adminInfo } = storeToRefs(store)
+const { adminInfo, sideWidth } = storeToRefs(store)
+const { handleSideWidth } = store
+
+const { isFullscreen, toggle } = useFullscreen()
+
+const handleRefresh = () => location.reload()
 
 const { handleLogout } = useLogout()
+
+const { formDrawerRef, form, rules, formRef, onSubmit, openRepasswordForm } = usePassword()
 </script>
 
 <template>
   <div class="f-header v-center">
     <RouterLink to="/">
-      <div class="f-center transition-all duration-500" style="width: 220px">
-        <IEpElementPlus />
-        <span class="text-2xl">极客空间</span>
+      <div class="f-center text-[1.5rem] transition-all duration-500" :style="{ width: sideWidth }">
+        <IEpElementPlus class="mr-3" />
+        <span v-show="sideWidth === '220px'">极客空间</span>
       </div>
     </RouterLink>
 
-    <div class="v-center">
-      <IEpFold />
+    <div @click="handleSideWidth" class="icon v-center">
+      <IEpFold v-if="sideWidth === '220px'" />
+      <IEpExpand v-else />
     </div>
 
     <div class="ml-auto v-center">
-      <IEpRefresh class="icon" />
-      <IEpFullScreen class="icon" />
+      <IEpRefresh @click="handleRefresh" class="icon" />
+      <div @click="toggle" class="icon v-center">
+        <IEpFullScreen v-if="!isFullscreen" />
+        <IEpAim v-else />
+      </div>
       <IEpPrinter class="icon" />
       <IEpSetting class="icon" />
 
@@ -32,21 +43,42 @@ const { handleLogout } = useLogout()
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>修改密码</el-dropdown-item>
+            <el-dropdown-item @click="openRepasswordForm">修改密码</el-dropdown-item>
             <el-dropdown-item @click="handleLogout">退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
     </div>
   </div>
+
+  <FormDrawer ref="formDrawerRef" title="修改密码" destroyOnClose @submit="onSubmit">
+    <el-form ref="formRef" :rules="rules" :model="form" label-width="80px">
+      <el-form-item prop="oldPassword" label="旧密码">
+        <el-input v-model="form.oldPassword" placeholder="请输入旧密码"></el-input>
+      </el-form-item>
+      <el-form-item prop="newPassword" label="新密码">
+        <el-input
+          type="password"
+          v-model="form.newPassword"
+          placeholder="请输入密码"
+          show-password
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="rePassword" label="确认密码">
+        <el-input
+          type="password"
+          v-model="form.rePassword"
+          placeholder="请输入确认密码"
+          show-password
+        ></el-input>
+      </el-form-item>
+    </el-form>
+  </FormDrawer>
 </template>
 
 <style scoped>
 .f-header {
   @apply bg-gradient-to-r from-cyan-600 to-sky-600 text-light-50 fixed top-0 left-0 right-0 h-16 shadow-xl shadow-gray-400;
-}
-.el-icon {
-  @apply mx-1;
 }
 .icon {
   @apply mx-1 cursor-pointer hover: opacity-70 text-xl;
